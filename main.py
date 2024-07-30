30c39e636c8ee1080984183e25ef407a034e42d9
Nishino1121 <47025764+Nishino1121@users.noreply.github.com>
2024-07-30 17:47:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ intents.voice_states = True
 intents.guilds = True
 
 client = discord.Client(intents=intents)
+excluded_channels = os.getenv("EXCLUDED_CHANNELS")
+if excluded_channels is None:
+    excluded_channels = []
+else:
+    excluded_channels = [int(x) for x in excluded_channels.split(',')]
 
 @client.event
 async def on_ready():
@@ -18,10 +23,18 @@ async def on_voice_state_update(member, before, after):
     channel_name = "入退室ログ"  # 通知したいチャンネルの名前
     channel = discord.utils.get(member.guild.channels, name=channel_name)
 
-    if before.channel is None and after.channel is not None:  # ボイスチャンネルに入った
-        await channel.send(f'{member.name} が {after.channel.name} に参加しました。')
-    elif before.channel is not None and after.channel is None:  # ボイスチャンネルから出た
-        await channel.send(f'{member.name} が {before.channel.name} から退出しました。')
+    if before.channel != after.channel:  # チャンネルが変わった
+        if before.channel is None:  # ボイスチャンネルに入った
+            if after.channel.id not in excluded_channels:
+                await channel.send(f'{member.name} が {after.channel.name} に参加しました。')
+        elif after.channel is None:  # ボイスチャンネルから出た
+            if before.channel.id not in excluded_channels:
+                await channel.send(f'{member.name} が {before.channel.name} から退出しました。')
+        else:  # ボイスチャンネルを移動した
+            if before.channel.id not in excluded_channels:
+                await channel.send(f'{member.name} が {before.channel.name} から退出しました。')
+            if after.channel.id not in excluded_channels:
+                await channel.send(f'{member.name} が {after.channel.name} に参加しました。')
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 # Web サーバの立ち上げ
